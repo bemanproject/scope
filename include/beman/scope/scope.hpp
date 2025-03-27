7fd7b811b395c24ce3d3ddeebd4bc658f5c30e64
Robert Andrzejuk <robert-andrzejuk@users.noreply.github.com>
2025-03-27 14:58:43 +0100

    fix ifdefs

--- include/beman/scope/scope.hpp
+++ include/beman/scope/scope.hpp
@@ -346,7 +346,8 @@ private:
 //==================================================================================================
 
 
-#ifdef __GNUC__ && !defined(__clang__)
+#ifdef defined(__GNUC__)
+
 // Due to gcc error `ExitFunc` cannot be `scope_exit_function`
 // error: no type named 'type' in 'struct std::invoke_result<beman::scope::Releasable<void> >'
 // using std::invocable instead
