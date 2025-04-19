9fd948eb901ffa550f780a240de5618dd44125eb
JeffGarland <jeff@crystalclearsoftware.com>
2025-04-19 06:25:29 -0700

    add tmp emacs to gitignore - unique resource file example

---
+++ examples/unique_resource-file.cpp
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <iostream>
+#include <memory>
+#include <cstdio>
+#include <beman/scope/scope.hpp>
+
+//clang-format off
+int main() {
+
+  {
+    auto file = beman::scope::unique_resource(
+        fopen("example.txt", "w"), // Acquire the FILE*
+        [](FILE* f) {
+            if (f) {
+                std::cout << "Closing file.\n";
+                fclose(f); // Release (cleanup) the resource
+            }
+        }
+    );
+
+    if (!file.get()) {
+        std::cerr << "Failed to open file.\n";
+        return 1;
+    }
+  }
+
+  // Resource is automatically released when `file` goes out of scope
+  std::cout << "File has been closed \n";
+
+}
+//clang-format on
